cba26a2ec94c71134543ea0af06ce564fab7fd83
Richard Tsibu <anointedone5976@gmail.com>
2023-05-20 04:43:46 -0700

    task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This the main entry
+ * Return: Always return 0 , which means success
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This is the point of entry
+ *
+ * return: Always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
