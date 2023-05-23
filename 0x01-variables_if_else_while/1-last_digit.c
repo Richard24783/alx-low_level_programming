c9e4c76ab656cee02bd6b8df9233a5ffa840403a
Richard Tsibu <anointedone5976@gmail.com>
2023-05-23 05:05:51 -0700

    hardthings

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ * and wether is greater than 5 , less 6 or 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int lastdigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastdigit = n % 10;
+
+	if (lastdigit > 5)
+	{
+	printf("Last digit of %d is %d is greater than 5\n", n, lastdigit);
+	}
+	else if (lastdigit == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main -This is  the main entry
+ *Return: Always return 0 (success)
+ */
+int main(void)
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i == 0, i < 26, 1++)
+
+	{
+		putchar (alp[i]);
+	}
+	putchar('h');
+	return (0);
