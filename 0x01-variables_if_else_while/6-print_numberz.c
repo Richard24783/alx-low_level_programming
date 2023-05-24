76cb01159e4df147adbd71161909e6bcecbe3b56
Richard Tsibu <anointedone5976@gmail.com>
2023-05-23 19:48:22 -0700

    add

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - This is the main entry
+ *Return: Always return 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
