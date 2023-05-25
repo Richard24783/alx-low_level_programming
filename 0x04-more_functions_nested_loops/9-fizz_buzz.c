9f5b381d8888a36240faee8560737b5e7298c9af
Richard Tsibu <anointedone5976@gmail.com>
2023-05-25 13:07:34 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,18 +13,14 @@ int main(void)
 
 	for (o = 1; o <= 100; o++)
 	{
-		if (o % 3 == 0 && o % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
+		if (o % 3 == 0)
+			printf("Fizz");
 		else if (o % 5 == 0)
-		{
 			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", o);
-		}
+		else if ((o % 3 != 0) && (o % 5 != 0))
+			printf("%d", o);
+		if (o != 100)
+			printf(" ");
 	}
 	printf("\n");
 
