daf89c7bb16796a4c3ca3744373dccf4c5011cca
Richard Tsibu <anointedone5976@gmail.com>
2023-06-02 05:31:46 -0700

    tasko

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - prints _putchar and new line
+ *Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
