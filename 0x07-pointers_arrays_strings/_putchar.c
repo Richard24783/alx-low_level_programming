b1805d526e30500daed9417734a45bb0c4f2c611
Richard Tsibu <anointedone5976@gmail.com>
2023-06-05 15:40:18 -0700

    task6

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,5 +1,5 @@
-#include " main.h"
-#include <unistd>
+#include "main.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -8,7 +8,7 @@
  * Return: on success 1.
  * on error, -1 is returned, and error is set appropriately.
  */
-
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }	
