8c9c95ffb240b2165653e2faaeb503662ce1990d
Richard Tsibu <anointedone5976@gmail.com>
2023-06-11 08:10:15 -0700

    static

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -3,7 +3,7 @@
  *_memset - fill a block of memory with a specific value
  *@s: starting address of memory to be filled
  *@b: the desired value
- *@n: number of bytes to be changed
+ *@n: number bytes to be changed
  *
  * Return: changed array with new value for n bytes
  */
@@ -11,7 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (n > 0; i++;)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
