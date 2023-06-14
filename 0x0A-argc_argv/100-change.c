c133d41644d94f9532170f4a2b6ffaae8ebe0c2f
Richard Tsibu <anointedone5976@gmail.com>
2023-06-14 10:06:15 -0700

    HEAD

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - This prints minimum no of coins to make
+* change for an amount of money.
+* @argc: The number of arguments passed.
+* @argv: The pointer toarray of size argc
+*
+* Return: 0 on success, otherwise 1.
+*/
+
+int main(int argc, char *argv[])
+{
+	int change, i;
+
+	change = 0;
+	if (argc == 2)
+	{
+		i = atoi(argv[1]);
+		if (i > 0)
+		{
+			for (; i >= 25; change++)
+			{
+			i = i - 25;
+			}
+			for (; i >= 10; change++)
+			{
+			i = i - 10;
+			}
+			for (; i >= 5; change++)
+			{
+			i = i - 5;
+			}
+			for (; i >= 2; change++)
+			{
+			i = i - 2;
+			}
+			for (; i >= 1; change++)
+			{
+			i = i  - 1;
+			}
+		}
+		printf("%d\n", change);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - This prints results of a multiplication
+* followed by a new line
+* @argc: The number of arguments passed.
+* @argv: array of size argc, containing arguments passed.
+*
+* Return: 0 on success otherwise 1.
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
