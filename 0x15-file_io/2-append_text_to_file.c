ab326967d2eedfcb4a03bf52cafcfc19b44d55ed
Richard24783 <anointedone5976@gmail.com>
2023-07-11 03:58:13 -0700

    file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,23 +13,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
-			;
-		if (write(fd, text_content, i) == -1)
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	close(fd);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
 	return (1);
 }
